8a7824a14d199a692218ad116d715141dea93237
Kevin O'Brien <kob123@hotmail.co.uk>
2019-08-03 16:40:24 +0100

    Styling React with Radium
    The Radium package allows you to use css pseudo-selectors and media queries as part of inline styles rather than traditional CSS.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+// Radium enables us to use css pseudo-selectors inline
+// StyleRoot enables us to use media-queries inline
+import Radium, {StyleRoot} from 'radium'; 
 import './App.css';
 import Person from './Person/Person';
 
@@ -56,12 +59,18 @@ class App extends Component {
   }
 
   render() {
+    // Just passed as a prop to the button
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color:  'black'
+      }
     }
 
     // persons will be reset each time the button is clicked because
@@ -82,22 +91,39 @@ class App extends Component {
           })}
         </div>
       )
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'white'
+      }
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}
-        >Toggle Persons
-        </button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={style}
+            onClick={this.togglePersonsHandler}
+          >Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+// We wrap person in a higer-order component that provides person with extra 
+// functionality e.g. can use css pseudo-classes in inline JS styling.
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Radium from 'radium';
 
 import './Person.css';
 
 const person = (props) => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
+
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
       {/* props.children accesses whatever text or HTML is between the React
       component opening and closing tags */}
@@ -14,4 +21,6 @@ const person = (props) => {
   )
 };
 
-export default person;
+// We wrap person in a higer-order component that provides person with extra 
+// functionality e.g. can use css pseudo-classes in inline JS styling.
+export default Radium(person);
