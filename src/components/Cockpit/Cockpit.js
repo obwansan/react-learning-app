72dff0b9051276517f875b1b84be30daae94a5fb
Kevin O'Brien <kob123@hotmail.co.uk>
2019-08-17 16:13:29 +0100

    Refs with React Hooks
    Need to use the useRef hook inside the useEffect hook so the reference is only used after the JSX has rendered.

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,23 +1,30 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 // CSS Modules: Loads the CSS rules in as a JS object called classes.
 // Can access individual rules as properties on the object.
 import classes from './Cockpit.css'
 
-const cockpit = (props) => {
-  // useEffect seems to do everything that the class-based component 
+const cockpit = props => {
+  const toggleBtnRef = useRef(null);
+
+  // The useEffect hook seems to do everything that the class-based component 
   // lifecycle methods componentDidMount and componentDidUpdate do 
-  // (it runs on every state update / rerender)
+  // (it runs after every state update / rerender cycle)
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
-    const timer = setTimeout(() => {
-      alert('Saved data to cloud!');
-    }, 1000);
+    // This clicks the button after the cockpit component renders. If we called it outside
+    // the useEffect() hook in a functional component, it would run before the JSX was rendered
+    // and so you'd get an error because the button element that toggleBtnRef refers to wouldn't 
+    // exist.
+    toggleBtnRef.current.click();
     return () => {
       // clearTimeout(timer); // If you click the 'Remove Cockpit' button before setTimeout runs, the cockpit component is unmounted, this useEffect cleanup return function runs, and the timeout is cleared before the alert can run.
       console.log('[Cockpit.js] cleanup work in useEffect'); // runs when the component unmounts
     };
-  // }, [props.persons]); // only calls useEffect if props.persons changes
-}, []); // only calls useEffect on first component render and component unmount  (equivalent of componentDidMount)
+  // }, [props.persons]); // Only calls useEffect if props.persons changes
+}, []); 
+// Passing an empty array as the second arrgument to useEffect() means it's only called on 
+// the first component render and on component unmount (i.e. equivalent of componentDidMount)
+// rather than on every component re-render cycle.
 
   const assignedClasses = [];
   let btnClass = '';
@@ -38,6 +45,7 @@ const cockpit = (props) => {
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(' ')}>This is really working!</p>
       <button 
+        ref={toggleBtnRef}
         className={btnClass}
         onClick={props.clicked}
       >Toggle Persons
